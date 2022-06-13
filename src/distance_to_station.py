7d899f1a0053ac962ee3f32ec94132ae487b4807
deepakvk0712 <90729944+deepakvk0712@users.noreply.github.com>
2022-06-13 13:22:10 -0400

    FAWN dataset added and distance_to_station function
    Added FAWN dataset 1998 - 2022 and started work on the distance_to_station function.

---
+++ src/distance_to_station.py
@@ -0,0 +1,19 @@
+"""
+Function Name: distance_to_station
+Input:
+Output:  
+Description: This function takes the latitude and longitude value of a given point nad returns the closest weather station to the location. The function checks in increasing distance bubbles. First, a check is made to see is there is any weather station within 5 kilometers to the location, then 10 kilometers, finally 20 kilometers.
+"""
+
+# Packages that need to be imported
+import geopy.distance
+coords_1 = (52.2296756, 21.0122287)
+coords_2 = (52.406374, 16.9251681)
+
+def distance_to_station(input_station, check_station):
+    # The two inputs are tuples that contain the station ID, station name, latitude and longitude
+    return geopy.distance.geodesic(coords_1, coords_2).km
+
+
+print(distance_to_station(coords_1, coords_2))
+
