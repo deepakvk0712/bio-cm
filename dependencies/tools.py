8b43944c0663826d09d3f40d6b08373af68fa266
deepakvk0712 <90729944+deepakvk0712@users.noreply.github.com>
2022-06-14 14:56:13 -0400

    Started writing tools and completed writing function to get distance
    Started writing tools functions and also finished writing the function to get distance between two coordinates.

---
+++ dependencies/tools.py
@@ -0,0 +1,19 @@
+import pandas as pd
+
+"""
+Function Name: convert excel file to csv file
+Input: path to excel file, path to store created csv file
+Output: None (csv file created at the specified path)
+Description: This function takes an excel file and converts it into a csv file
+"""
+
+path_to_excel = "/Users/deepak/Library/Containers/com.microsoft.Excel/Data/Desktop/biocm/Data/FAWN-stations.xlsx"
+path_to_csv = "/Users/deepak/Desktop/biocm/Code/bio-cm/data/FAWN_stations.csv"
+
+def convert_excel_to_csv(pathToExcelFile, pathToCSVFile):
+    read_file = pd.read_excel(pathToExcelFile)
+    read_file.to_csv(pathToCSVFile, index = None, header=True)
+
+
+
+convert_excel_to_csv(path_to_excel, path_to_csv)
--- src/distance_to_station.py
+++ src/distance_to_station.py
@@ -7,13 +7,18 @@ Description: This function takes the latitude and longitude value of a given poi
 
 # Packages that need to be imported
 import geopy.distance
-coords_1 = (52.2296756, 21.0122287)
-coords_2 = (52.406374, 16.9251681)
 
-def distance_to_station(input_station, check_station):
+# coords_1 = (52.2296756, 21.0122287)
+# coords_2 = (52.406374, 16.9251681)
+
+def distance_to_station(inputStation, checkStation):
     # The two inputs are tuples that contain the station ID, station name, latitude and longitude
-    return geopy.distance.geodesic(coords_1, coords_2).km
+    return geopy.distance.geodesic(inputStation, checkStation).km
+
+def get_closest_station(inputStation, allStations):
+    # The function takes two inputs. First tuple contains location longitude and latitude and the other is a list of stations - contains station ID, station name, latitude and longitude of the station.
 
 
-print(distance_to_station(coords_1, coords_2))
+
+# print(distance_to_station(coords_1, coords_2))
 
